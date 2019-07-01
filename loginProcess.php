df5e1101769fdc3abf2cf760261ba430f5c1a58e
carolinepar <51763164+carolinepar@users.noreply.github.com>
2019-07-01 17:27:45 -0400

    Add files via upload

--- loginProcess.php
+++ loginProcess.php
@@ -1,7 +1,21 @@
 <?php
 
+include('connect_db.php');
+
 $username = $_POST['username'];
 $password = $_POST['password'];
+
+$getUsers = "SELECT * FROM users WHERE username = '$username'" ;
+$resultUsers = mysqli_query($connection,$getUsers);
+
+
+
+if(!$resultUsers->data_seek(0)){
+	
+	echo 'Invalid username';
+	//then go back to login page
+	
+}
 
 
 
