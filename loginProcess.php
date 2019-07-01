d7527f238667339fdcc45662241e02fa2bfb0caf
carolinepar <51763164+carolinepar@users.noreply.github.com>
2019-06-30 20:28:14 -0400

    Added login process

---
+++ loginProcess.php
@@ -0,0 +1,29 @@
+<?php
+
+$username = $_POST[userName];
+
+$pass = $_POST[password];
+
+
+function makelink($mail) {
+
+
+//echo "<a href='mailto:$mail'>$mail</a>";
+
+
+
+
+
+}
+
+//echo $username . " | " . $emailAddress;
+
+//echo "Thanks for this comment " . $username . "<br/>" ;
+//echo "<p> $userComments </p>";
+//echo "Thanks we will reply to ";
+//echo makelink($emailAddress);
+
+
+
+
+?>
